dd06a86131b283dd483d9719a91eb3e4a5e5ec53
Corey Earwood <iamcgn+github@gmail.com>
2013-08-07 21:10:24 -0600

    CollectionDetail layout with navigation to item detail

--- RLLCollectionViewDelegate.m
+++ RLLCollectionViewDelegate.m
@@ -11,22 +11,22 @@
 @implementation RLLCollectionViewDelegate
 
 - (void)setDelegate:(id)delegate {
-    _delegate = delegate;
-    
-    //If _delegate isn't set first the surrogate setup breaks for some methods like scrollViewDidScroll: which only check once and cache the results
-    if (self.collectionView) {
-        self.collectionView = self.collectionView;
-    }
+  _delegate = delegate;
+  
+  //If _delegate isn't set first the surrogate setup breaks for some methods like scrollViewDidScroll: which only check once and cache the results
+  if (self.collectionView) {
+    self.collectionView = self.collectionView;
+  }
 }
 
 - (void)setCollectionView:(UICollectionView *)collectionView {
-    _collectionView.delegate = nil;
-    _collectionView.dataSource = nil;
-    
-    _collectionView = collectionView;
-    
-    _collectionView.delegate = (id<UICollectionViewDelegate>)self;
-    _collectionView.dataSource = (id<UICollectionViewDataSource>)self;
+  _collectionView.delegate = nil;
+  _collectionView.dataSource = nil;
+  
+  _collectionView = collectionView;
+  
+  _collectionView.delegate = (id<UICollectionViewDelegate>)self;
+  _collectionView.dataSource = (id<UICollectionViewDataSource>)self;
 }
 
 - (void)updatePageControllerCurrentPage {
@@ -40,23 +40,26 @@
 #pragma mark - UICollectionView
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    if ([_delegate respondsToSelector:_cmd]) {
-        return [_delegate numberOfSectionsInCollectionView:collectionView];
-    }
-    
-    return self.topArrayContainsSections ? [self.dataSource count] : 1;
+  if ([_delegate respondsToSelector:_cmd]) {
+    return [_delegate numberOfSectionsInCollectionView:collectionView];
+  }
+  
+  return self.topArrayContainsSections ? [self.dataSource count] : 1;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    if ([_delegate respondsToSelector:_cmd]) {
-        return [_delegate collectionView:collectionView numberOfItemsInSection:section];
-    }
-    
-    return self.topArrayContainsSections ? [self.dataSource[section] count] : [self.dataSource count];
+  if ([_delegate respondsToSelector:_cmd]) {
+    return [_delegate collectionView:collectionView numberOfItemsInSection:section];
+  }
+  
+  return self.topArrayContainsSections ? [self.dataSource[section] count] : [self.dataSource count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+  if ([_delegate respondsToSelector:_cmd]) {
+    return [_delegate collectionView:collectionView cellForItemAtIndexPath:indexPath];
+  }
+  return nil;
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
@@ -72,32 +75,32 @@
 #pragma mark - Setup _delegate as surrogate object
 
 - (BOOL)respondsToSelector:(SEL)aSelector {
-    if ([_delegate respondsToSelector:aSelector]) {
-        return YES;
-    } else {
-        return [super respondsToSelector:aSelector];
-    }
+  if ([_delegate respondsToSelector:aSelector]) {
+    return YES;
+  } else {
+    return [super respondsToSelector:aSelector];
+  }
 }
 
 - (BOOL)conformsToProtocol:(Protocol *)aProtocol {
-    return [_delegate conformsToProtocol:aProtocol];
+  return [_delegate conformsToProtocol:aProtocol];
 }
 
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-    if ([_delegate respondsToSelector:aSelector]) {
-        return [_delegate methodSignatureForSelector:aSelector];
-    }
-    
-    return [super methodSignatureForSelector:aSelector];
+  if ([_delegate respondsToSelector:aSelector]) {
+    return [_delegate methodSignatureForSelector:aSelector];
+  }
+  
+  return [super methodSignatureForSelector:aSelector];
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
-    if ([_delegate respondsToSelector:anInvocation.selector]) {
-        [anInvocation invokeWithTarget:_delegate];
-    } else {
-        [super forwardInvocation:anInvocation];
-    }
+  if ([_delegate respondsToSelector:anInvocation.selector]) {
+    [anInvocation invokeWithTarget:_delegate];
+  } else {
+    [super forwardInvocation:anInvocation];
+  }
 }
 
 @end
