3acc1fd2764ef85e480d7ff9eff6cdbca80aea24
Corey Earwood <iamcgn+github@gmail.com>
2013-09-12 11:57:15 -0600

    Add Mutable Dictionary proxy class that ignores setting object nil for any key

---
+++ RLLMutableDictionary.h
@@ -0,0 +1,15 @@
+//
+//  RLLMutableDictionary.h
+//  Fovo
+//
+//  Created by Corey Earwood on 9/12/13.
+//  Copyright (c) 2013 Seedbar, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RLLMutableDictionary : NSProxy
+
+- (id)init;
+
+@end
---
+++ RLLMutableDictionary.m
@@ -0,0 +1,42 @@
+//
+//  RLLMutableDictionary.m
+//  Fovo
+//
+//  Created by Corey Earwood on 9/12/13.
+//  Copyright (c) 2013 Seedbar, LLC. All rights reserved.
+//
+
+#import "RLLMutableDictionary.h"
+#import <objc/runtime.h>
+
+@interface RLLMutableDictionary ()
+
+@property (nonatomic, strong) NSMutableDictionary *proxy;
+
+@end
+
+@implementation RLLMutableDictionary
+
+- (id)init {
+  _proxy = [NSMutableDictionary new];
+  return self;
+}
+
+//Prevent exception caused by attempting to add nil object.  This way we don't have to add a conditional for every key that may not exist from a remote API
+- (void)forwardInvocation:(NSInvocation *)invocation {
+  void *obj = NULL;
+  
+  [invocation getArgument:&obj atIndex:2];
+  
+  if (invocation.selector == @selector(setObject:forKeyedSubscript:) && obj == NULL) {
+    return;
+  } else {
+    [invocation invokeWithTarget:_proxy];
+  }
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+  return [_proxy methodSignatureForSelector:aSelector];
+}
+
+@end
